e296e381735355297e33fee1194ec6b2b3d9d59c
Cai <cloud_chl@163.com>
2024-08-20 22:35:49 +0800

    FastApi-06_Request and Response

--- FastAPI/06_Request and Response/main.py
+++ FastAPI/06_Request and Response/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
 from apps.app01 import app01
+from apps.app02 import app02
 
 app = FastAPI()
 
 app.include_router(app01, tags=["01 路径参数"])
+app.include_router(app02, tags=["02 路径参数"])
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=8080, reload=True)
