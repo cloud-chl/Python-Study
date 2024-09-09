04db4b75e55960cf8247630365918d25142ff337
Cai <cloud_chl@163.com>
2024-09-09 22:41:05 +0800

    FastApi-08_ORM

--- FastAPI/08_ORM/main.py
+++ FastAPI/08_ORM/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 import uvicorn
 from tortoise.contrib.fastapi import register_tortoise
 from settings import TORTOISE_ORM
+from api.student import student_api
 
 app = FastAPI()
+
+app.include_router(student_api, prefix="/student")
 
 register_tortoise(
     app = app,
@@ -12,7 +15,7 @@ register_tortoise(
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=8080, reload=True)
-
+ 
     # aerich init -t settings.TORTOISE_ORM
     # aerich init-db
     # aerich migrate
